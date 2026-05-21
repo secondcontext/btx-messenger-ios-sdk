c32076721caa0a48eb4683188723c7d3a93486fe
farukparhat <faruk.parhat@outlook.com>
2026-05-21 19:04:14 +0200

    Release BTXClientKit 0.2.6

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BTXClientKit",
-            url: "https://github.com/secondcontext/btx-ios-sdk/releases/download/0.2.5/BTXClientKit.xcframework.zip",
-            checksum: "21edb437515f9fcea78b6a9913ca8d9f5aa8d6ea4ba6f3dbf33bc07f2e41e19d"
+            url: "https://github.com/secondcontext/btx-ios-sdk/releases/download/0.2.6/BTXClientKit.xcframework.zip",
+            checksum: "df161c70741aa315ece73c9bfe6f445b105e0891bf64886c5b7bcb66c3b96493"
         ),
     ]
 )
